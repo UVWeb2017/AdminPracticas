9c32199d74836ea4cac0cd32c65a365faf988407
Drowlex <angelfenix96@gmail.com>
2017-05-15 15:20:59 -0500

    Script agregados

---
+++ SQL/Add_Carreras.sql
@@ -0,0 +1,12 @@
+insert into Carreras(id,Nombre)
+values(1,"Ingeniería Civil"),
+(2,"Ingeniería Eléctrica"),
+(3,"Ingeniería Electronica Y Comunicaciones"),
+(4,"Ingeniería Industrial"),
+(5,"Ingeniería Informática"),
+(6,"Ingeniería Mecánica"),
+(7,"Ingeniería Metalurgica"),
+(8,"Ingeniería Naval"),
+(9,"Ingeniería Quimica"),
+(10,"Ingeniería Topografica Geodésica");
+select *from Carreras;
